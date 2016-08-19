cb23347a11742c4ee86e92d884ce83b2a81a0a7e
Wuang Qin <wuang@leohealth.com>
2016-08-19 15:17:34 -0400

    add style on ask-an-expert

--- scripts/js/disqus.js
+++ scripts/js/disqus.js
@@ -7,7 +7,7 @@
   if (window.location.pathname.includes('ask-an-expert')){
     var a = document.createElement('div');
     a.id = 'disqus_thread';
-    var b = document.getElementsByClassName('container-inner')[1];
+    var b = document.getElementsByClassName('content-column')[0];
     b.appendChild(a);
   }
   var d = document, s = d.createElement('script');
