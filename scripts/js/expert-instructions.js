3617b01f142a0d0be4a3c8a86eb7d3e814b4e416
Nayan Jain <nayan@leohealth.com>
2016-10-10 15:01:08 -0400

    no issue - refactor

--- scripts/js/expert-instructions.js
+++ scripts/js/expert-instructions.js
@@ -1,6 +1,7 @@
 var showInstructions = function(expert_first_name, live_date, live_date_string, live_time_string, expert_pronoun) {
 	var live = new Date(live_date);
 	var today = new Date();
-
-	$("#instructions").html(expert_first_name +' will be answering questions from ' + live_time_string + (live.setHours(0,0,0,0) == today.setHours(0,0,0,0)? 'today' : ('on' +live_date_string) +'. Send your questions to '+expert_pronoun+' using the box below and tune in for the real-time discussion.');
+	var dateString = (live.setHours(0,0,0,0) == today.setHours(0,0,0,0) ? 'today' : ('on ' +live_date_string));
+	$("#instructions").html(expert_first_name +' will be answering questions from ' +
+		live_time_string + ' ' + dateString +'. Send your questions to '+expert_pronoun+' using the box below and tune in for the real-time discussion.');
 };
